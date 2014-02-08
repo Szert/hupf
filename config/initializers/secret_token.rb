243eda7025b47e619e0b07d24a0538b12fee92e9
Szert <karkusm@hotmail.com>
2014-02-08 15:40:23 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hupf::Application.config.secret_key_base = 'c2d5da3d1c23e503259f63c39d4e16bf6c13e662900a1b23d98046bd89c54733b0a5668daeee37a11accb4118eaefd37cdf475b7ab22d41cc04fbef4fd8dd137'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hupf::Application.config.session_store :cookie_store, key: '_hupf_session'
